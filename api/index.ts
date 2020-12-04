22c49c111b79791764cd73ea284eb796efe96c36
ArsalaBangash <arsalabangash@gmail.com>
2020-12-04 22:13:53 +0500

    Reads and writes to database working

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,8 @@
 // Loads required libraries
+require("dotenv").config()
 var ParseDashboard = require("parse-dashboard");
 const ParseServer = require("parse-server").ParseServer;
+const Parse = require('parse/node');
 var path = require("path");
 import express from "express";
 
@@ -12,19 +14,26 @@ if (!databaseUri) {
   console.log("DATABASE_URI not specified, falling back to localhost.");
 }
 
+Parse.initialize("template-backend-parse-express-ts", process.env.APP_MASTER_KEY);
+Parse.serverURL = `http://${BASE_URL}:${PORT}/api`
+
 const parseApi = new ParseServer({
   databaseURI: databaseUri,
-  appId: process.env.APP_ID || "parse-express-backend-template",
+  appId: process.env.APP_ID || "template-backend-parse-express-ts",
   masterKey: process.env.APP_MASTER_KEY || "",
   serverURL: process.env.SERVER_URL || `http://${BASE_URL}:${PORT}/api`,
+  verbose: true
 });
+
+const masterKey= process.env.APP_MASTER_KEY || ""
+console.log(masterKey)
 
 var parseDashboard = new ParseDashboard(
   {
     apps: [
       {
         serverURL: `http://${BASE_URL}:${PORT}/api`,
-        appId: "parse-express-backend-template",
+        appId: "template-backend-parse-express-ts",
         masterKey: process.env.APP_MASTER_KEY || "",
         appName: "Grey Software Parse Backend Template",
       },
@@ -48,6 +57,28 @@ app.get("/", (_, res) =>
   res.send("Your backend is live! Visit /dashboard for more details!")
 );
 
+function populateDbWithTestData() {
+  const GameScore = Parse.Object.extend("GameScore");
+  const gameScore = new GameScore();
+
+  gameScore.set("score", 1337);
+  gameScore.set("playerName", "Sean Plott");
+
+  gameScore.save().then(
+    (gameScore: any) => {
+      // Execute any logic that should take place after the object is saved.
+      console.log("New object created with objectId: " + gameScore.id);
+    },
+    (error: any) => {
+      // Execute any logic that should take place if the save fails.
+      // error is a Parse.Error with an error code and message.
+      console.log("Failed to create new object, with error code: " + error);
+    }
+  );
+}
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://${BASE_URL}:${PORT}`);
+  populateDbWithTestData()
+  
 });
